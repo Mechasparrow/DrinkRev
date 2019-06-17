744c66e7470a85ab44849489e05540111514a783
Mechasparrow <mikipux7@gmail.com>
2019-06-16 20:48:42 -0500

    Designed out basic home page

--- drinkreviews/views.py
+++ drinkreviews/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, here are some drinks")
+    return render(request, 'drinkreviews/index.html',{})
