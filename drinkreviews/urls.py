d3c62be845a45ab8fb9d6d115cff3547253bcc96
Mechasparrow <mikipux7@gmail.com>
2019-06-20 13:21:22 -0500

    scaffolded out view drink page view

--- drinkreviews/urls.py
+++ drinkreviews/urls.py
@@ -6,5 +6,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('view-drink', views.view, name = 'view')
 ]
--- drinkreviews/views.py
+++ drinkreviews/views.py
@@ -6,3 +6,6 @@ from django.template import loader
 # Create your views here.
 def index(request):
     return render(request, 'drinkreviews/index.html',{})
+
+def view(request):
+    return render(request, 'drinkreviews/view_drink.html', {})
